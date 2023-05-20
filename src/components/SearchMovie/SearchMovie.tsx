3147d807ac6c0d4c8bea117cd59ef0e2eceb0f74
32Valik92 <valokstrukalo@gmail.com>
2023-05-20 23:25:27 +0300

    part12: fixed pagination after successful search

--- src/components/SearchMovie/SearchMovie.tsx
+++ src/components/SearchMovie/SearchMovie.tsx
@@ -5,10 +5,11 @@ import {useAppDispatch, useAppSelector} from "../../hooks";
 import {MoviesListCard} from "../MoviesListCard/MoviesListCard";
 import {movieActions} from "../../redux";
 import './SearchMovie.css';
+import {Loading} from "../Loading/Loading";
 
 
 const SearchMovie: FC = () => {
-    const {searchMovies, searchTrigger, page, searchWords} = useAppSelector(state => state.movieReducer);
+    const {searchMovies, searchTrigger, page, searchWords, isLoading} = useAppSelector(state => state.movieReducer);
     const dispatch = useAppDispatch();
     const [query, setQuery] = useSearchParams({page: '1'});
 
@@ -28,32 +29,37 @@ const SearchMovie: FC = () => {
 
     return (
 
-        // If we don't have search movies, and we never click on button to send search words
-        !searchMovies.length && searchTrigger
+        // If we are in pending status we have spinner else we have our component
+        isLoading
             ?
-            <div className={'resultFoundMovie'}>
-                Sorry. We can't find these movies by keyword "{searchWords.searchWords}"
-            </div>
+            <Loading/>
             :
-            // If we have search words in our store for finding
-            searchWords.searchWords &&
-            <div className={'moviesList'}>
-
+            // If we don't have search movies, and we never click on button to send search words
+            !searchMovies.length && searchTrigger
+                ?
                 <div className={'resultFoundMovie'}>
-                    We found these movies by keyword "{searchWords.searchWords}"
+                    Sorry. We can't find these movies by keyword "{searchWords.searchWords}"
                 </div>
+                :
+                // If we have search words in our store for finding
+                searchWords.searchWords &&
+                <div className={'moviesList'}>
 
-                <div className={'movieCards'}>
-                    {/* Showing our searched movies */}
-                    {searchMovies.map(movie => <MoviesListCard key={movie.id} movie={movie}/>)}
-                </div>
+                    <div className={'resultFoundMovie'}>
+                        We found these movies by keyword "{searchWords.searchWords}"
+                    </div>
 
-                {/* Pagination */}
-                <div className={'pagination'}>
-                    <button disabled={page === 1} onClick={(): void => paginationFunc('prev')}> prev page</button>
-                    <button disabled={page === 500} onClick={(): void => paginationFunc('next')}> next page</button>
+                    <div className={'movieCards'}>
+                        {/* Showing our searched movies */}
+                        {searchMovies.map(movie => <MoviesListCard key={movie.id} movie={movie}/>)}
+                    </div>
+
+                    {/* Pagination */}
+                    <div className={'pagination'}>
+                        <button disabled={page === 1} onClick={(): void => paginationFunc('prev')}> prev page</button>
+                        <button disabled={page === 500} onClick={(): void => paginationFunc('next')}> next page</button>
+                    </div>
                 </div>
-            </div>
     );
 };
 
